6ffeaa8eb18dfd29b8863c3976b39fafce114736
midnightcrow00 <orsolyaalbert1@gmail.com>
2024-12-10 11:57:03 +0100

    javitva

--- laravel11-react-frontend/src/contexts/AuthContext.js
+++ laravel11-react-frontend/src/contexts/AuthContext.js
@@ -1,42 +1,44 @@
-import { useNavigate } from "react-router";
 import { myAxios } from "../api/myAxios"
 import { createContext, useState } from "react";
+import { useNavigate } from "react-router";
 
-export const AuthContext=createContext("")
-export const AuthProvider=(children)=>{
-
-    const[user, setUser]= useState("")
-    const navigate = useNavigate()
+export const AuthContext=createContext("");
+export const AuthProvider = ({ children }) => {
+  
+    const [user, setUser] = useState("");
+    const navigacio =useNavigate();
     const csrf = () => myAxios.get("/sanctum/csrf-cookie")
 
-  const regisztacio = async({...adat})=>{
-    await csrf();
-    try{
-      await myAxios.post("/register", adat)
-      getUser()
-    }catch(error){
-      console.log(error)
-      navigate("/")
+    const regisztacio = async({...adat})=>{
+      await csrf();
+      try{
+        await myAxios.post("/register", adat)
+        getUser();
+        navigacio("/") //Home komponenshez vezet
+      }catch(error){
+        console.log(error)
+      }
     }
-  }
 
-  const logout = async({...adat})=>{
-    await csrf();
-    try{
-      await myAxios.post("/logout", adat)
-    }catch(error){
-      console.log(error)
-      navigate("/login")
+    const logout =  async({...adat})=>{
+        await csrf();
+        try{
+          await myAxios.post("/logout", adat)
+          navigacio("/login") //Login komponenshez vezet
+        }catch(error){
+          console.log(error)
+        }
+      }
+  
+    const getUser = async () => {
+      const { data } = await myAxios.get("/api/user");
+      console.log(data);
+      setUser(data);
     }
-  }
 
-  const getUser = async()=> {
-    const {data} = await myAxios.get("/api/user")
-    console.log(data)
-    setUser(data)
-  }
-
-    return <AuthContext.AuthProvider value={{regisztacio,user, logout}}>
-        {{children}}
-    </AuthContext.AuthProvider>
+    return (
+        <AuthContext.Provider value={{ regisztacio, user, logout}}>
+          {children}
+        </AuthContext.Provider>
+      );
 }
